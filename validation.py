bdbb1132cf8306d0c0b557ce8781e9cb160573a7
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2025-02-11 17:38:39 +0000

    save

--- validation.py
+++ validation.py
@@ -1,4 +1,5 @@
 # validation.py
+import ast
 
 def is_valid_student_option(student_option, valid_options):
     """Check if the student_option is in the list of valid options."""
@@ -8,7 +9,7 @@ def read_valid_options(file_path):
     """Read the list of valid student options from the file."""
     try:
         with open(file_path, "r") as file:
-            valid_options = eval(file.read().strip())
+            valid_options = ast.literal_eval(file.read().strip())
             return valid_options
     except FileNotFoundError:
         print(f"File '{file_path}' not found.")
