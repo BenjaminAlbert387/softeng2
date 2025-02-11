9b4320f471a97664492b6605d213cb58985f14b2
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2025-02-11 15:07:16 +0000

    continue editing

--- testing2.py
+++ testing2.py
@@ -3,16 +3,17 @@ grades = ["50", "38", "77"]
 
 print(students[0] + " " + grades [0])
 
-first_option = input("Do you want to access students or modules")
+first_option = input("Do you want to access students or modules: ")
 
-while first_option == False:
-    if first_option == "students":
-        first_option == True
-        print("Loading students archive...")
+def OptionFunction():
+    print("Invalid option. Please try again.")
+    OptionFunction()
+#while first_option == False:
+if first_option == "students":
+    first_option = True
+    print("Loading students archive...")
         
-    elif first_option == "modules":
-        first_option = True
-        print("Loading modules")
+elif first_option == "modules":
+    first_option = True
+    print("Loading modules")
     
-    else:
-        print("Invalid option")
