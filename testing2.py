6899aadd2ea62e03e34863f8a76555aedb431799
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2025-02-11 15:00:51 +0000

    added while loop

--- testing2.py
+++ testing2.py
@@ -2,3 +2,17 @@ students = ["Adam", "Billy", "Carl"]
 grades = ["50", "38", "77"]
 
 print(students[0] + " " + grades [0])
+
+first_option = input("Do you want to access students or modules")
+
+while first_option == False:
+    if first_option == "students":
+        first_option == True
+        print("Loading students archive...")
+        
+    elif first_option == "modules":
+        first_option = True
+        print("Loading modules")
+    
+    else:
+        print("Invalid option")
