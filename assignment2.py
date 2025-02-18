3f2c357527195d75729de749b1b43abaeaa39211
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2025-02-18 22:23:39 +0000

    save

--- assignment2.py
+++ assignment2.py
@@ -1,18 +1,22 @@
 with open('studentid.txt') as file1, \
-     open('studentnames.txt') as file2, \
-     open('programmingmodule.txt') as file3:
+    open('studentnames.txt') as file2, \
+    open('programmingmodule.txt') as file3, \
+    open('ethicsmodule.txt') as file4:
 
     lines = file1.readlines()
     lines_2 = file2.readlines()
     lines_3 = file3.readlines()
+    lines_4 = file4.readlines()
 
     lns = [line.strip()for line in lines]
     lns_2 = [line.strip() for line in lines_2]
     lns_3 = [line.strip() for line in lines_3]
+    lns_3 = [line.strip() for line in lines_4]
 
     lns = list(map(int, lns))
     lns_2 = list(map(str, lns_2))
     lns_3 = list(map(int, lns_3))
+    lns_4 = list(map(int, lns_4))
 
     def OptionFunction():
         print("Type students, modules or averages.")
@@ -30,6 +34,7 @@ if first_option == "students":
         print(identifier_check)
         print("Name: " + lns_2[lns.index(identifier_check)])
         print("Programming Module: " + str(lns_3[lns.index(identifier_check)]))
+        print("Ethics Module: " + str(lns_4[lns.index(identifier_check)]))
     else:
         print("Invalid number")
     
