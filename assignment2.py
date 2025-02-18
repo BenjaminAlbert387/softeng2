3c782df4eea1aa8064ec4e8cc4b44deedc1b8f0b
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2025-02-18 15:14:06 +0000

    save

--- assignment2.py
+++ assignment2.py
@@ -1,9 +1,18 @@
-student_file = open("assignment2file.txt", "r")
 
 student_check = input("Please enter the name of the student you want to find: ")
 
 with open('assignment2file.txt') as f:
     if student_check in f.read():
         print('Yes, student is there')
+
+        class Person:
+            def __init__(self, name):
+                self.name = name
+                #self.identifier = identifier
+
+        p1 = Person(student_check)
+
+        print(p1.name)
+
     else:
         print('No, invalid')
