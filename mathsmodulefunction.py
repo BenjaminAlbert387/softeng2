28621ac3eefe6d44480dc99aa876b7b12ec42ff9
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2025-02-18 22:55:45 +0000

    save

--- mathsmodulefunction.py
+++ mathsmodulefunction.py
@@ -16,7 +16,7 @@ def maths_module_function(identifier_check, lns):
         lns_5 = [line.strip() for line in lines_5]
         lns_5 = list(map(int, lns_5))
         
-        if identifier_check in lns_5:
+        if identifier_check in lns:
             print("Maths Module: " + str(lns_5[lns.index(identifier_check)]))
         else:
             print("Invalid")
