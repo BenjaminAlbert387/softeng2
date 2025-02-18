2a5685d172c30252cd2592f0a916383c398c7be8
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2025-02-18 16:09:45 +0000

    save

--- assignment2.py
+++ assignment2.py
@@ -7,31 +7,21 @@
 #student_id_list.append(student_identifers.read())
 #print(student_id_list)
 
-with open('assignment2file.txt') as f, open('studentid.txt') as f2:
-    #if student_check in f.read():
-        #print('Yes, student is there')
+with open('studentid.txt') as file1, open('studentnames.txt') as file2:
+    lines = file1.readlines()
+    lines_2 = file2.readlines()
 
-        #class Person:
-            #def __init__(self, name):
-                #self.name = name
-                #self.identifier = identifier
-
-        #p1 = Person(student_check)
-
-        #print(p1.name)
-
-    #else:
-        #print('No, invalid')
-
-    lines = f.readlines()
-    lines_2 = f2.readlines()
     lns = [line.strip()for line in lines]
     lns_2 = [line.strip() for line in lines_2]
 
-    lns = list(map(str, lns))
-    lns_2 = list(map(int, lns_2))
+    lns = list(map(int, lns))
+    lns_2 = list(map(str, lns_2))
+    print(lns)
 
-    student_check = input("Please enter the name of the student you want to find: ")
-    if student_check in lns:
-        print(student_check)
-        print(lns_2[lns.index(student_check)])
+    identifier_check = int(input("Please enter the ID number of the student you want to find: "))
+
+    if identifier_check in lns:
+        print(identifier_check)
+        print("Name: " + lns_2[lns.index(identifier_check)])
+    else:
+        print("Invalid number")
