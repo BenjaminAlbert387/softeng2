24781da0d1b2f5aa0c5cb927d3290ff5bd8f5a48
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2025-02-18 14:49:08 +0000

    save

--- assignment2.py
+++ assignment2.py
@@ -1,4 +1,12 @@
 student_file = open("assignment2file.txt", "r")
 
+
+class Student:
+    def __init__(student, name, identifier):
+        student.name = name
+        student.identifier = identifier
+
+p1 = Student("Alice", 1001)
+
 print(p1.name)
-print(p1.identifier)
+print(student_file.readlines())
