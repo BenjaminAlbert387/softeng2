705de1e1f483584554b1c7c2d1e5156d74b14b7e
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2025-02-18 22:44:32 +0000

    save

--- mathsmodulefunction.py
+++ mathsmodulefunction.py
@@ -1,3 +1,15 @@
+def student_id_function():
+    with open('studentid.txt') as file1:
+        lines_1 = file1.readlines()
+        lns = [line.strip() for line in lines_1]
+        lns = list(map(int, lns))
+        
+        identifier_check = int(input("Please enter the ID number of the student you want to find: "))
+
+        if identifier_check in lns:
+            print(identifier_check)
+            return identifier_check
+
 def maths_module_function():
     with open('ethicsmodule.txt') as file5:
         lines_5 = file5.readlines()
